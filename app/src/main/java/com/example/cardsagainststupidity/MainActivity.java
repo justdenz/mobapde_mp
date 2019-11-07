02a0b2048a3dd67994185704f3db8cfed7bf9463
Denzel Co <denz3550@gmail.com>
2019-11-07 13:43:32 +0800

    created project

---
+++ app/src/main/java/com/example/cardsagainststupidity/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.cardsagainststupidity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
