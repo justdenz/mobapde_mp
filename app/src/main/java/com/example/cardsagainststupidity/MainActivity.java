fb4d1fa19b59bff4ee7df999a3936acf44d83c46
Denzel Co <denz3550@gmail.com>
2019-11-18 01:14:13 +0800

    recycler view for quiz card

--- app/src/main/java/com/example/cardsagainststupidity/MainActivity.java
+++ app/src/main/java/com/example/cardsagainststupidity/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.cardsagainststupidity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initRecyclerView();
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.recycler_view_pinned);
+        QuizCardAdapter quizCardAdapter = new QuizCardAdapter(this);
+        recyclerView.setAdapter(quizCardAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/example/cardsagainststupidity/QuizCardAdapter.java
@@ -0,0 +1,61 @@
+package com.example.cardsagainststupidity;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class QuizCardAdapter extends RecyclerView.Adapter<QuizCardAdapter.ViewHolder> {
+
+    private static final String TAG = "QuizCardAdapter";
+
+    private Context context;
+
+    public QuizCardAdapter(Context context) {
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.quiz_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Log.d(TAG, "onBindViewHolder: called");
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+//        TextView quizTitle, quizSubject, quizDescription, quizFlashCardQuantity, quizAuthor;
+//        CardView quizCard;
+//        Button takeQuizBtn, pinQuizBtn;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+//            quizTitle = itemView.findViewById(R.id.quizTitle);
+//            quizSubject = itemView.findViewById(R.id.quizSubject);
+//            quizDescription = itemView.findViewById(R.id.quizDescription);
+//            quizFlashCardQuantity = itemView.findViewById(R.id.quizFlashCardQuantity);
+//            quizAuthor = itemView.findViewById(R.id.quizAuthor);
+//            quizCard = itemView.findViewById(R.id.quizCard);
+//            takeQuizBtn = itemView.findViewById(R.id.takeQuizBtn);
+//            pinQuizBtn = itemView.findViewById(R.id.pinQuizBtn);
+        }
+    }
+}
