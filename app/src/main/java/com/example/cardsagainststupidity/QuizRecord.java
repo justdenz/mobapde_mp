66ded9c182ed25d6c1bfb1428d7ab7fb3167d46f
gabtan99 <gabtan99@gmail.com>
2019-11-18 12:28:22 +0800

    added objects

---
+++ app/src/main/java/com/example/cardsagainststupidity/QuizRecord.java
@@ -0,0 +1,43 @@
+package com.example.cardsagainststupidity;
+
+public class QuizRecord {
+
+	private int quizID;
+	private float scorePercentage;
+	private int duration;
+
+	public QuizRecord () {
+
+	}
+
+	public QuizRecord (int id, float s, int d) {
+		this.quizID = id;
+		this.scorePercentage = s;
+		this.duration = d;
+	}
+
+	public int getQuizID() {
+		return quizID;
+	}
+
+	public void setQuizID(int quizID) {
+		this.quizID = quizID;
+	}
+
+	public float getScorePercentage() {
+		return scorePercentage;
+	}
+
+	public void setScorePercentage(float scorePercentage) {
+		this.scorePercentage = scorePercentage;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+}
---
+++ app/src/main/java/com/example/cardsagainststupidity/UserStatsHelper.java
@@ -0,0 +1,52 @@
+package com.example.cardsagainststupidity;
+
+import java.util.ArrayList;
+
+public class UserStatsHelper {
+
+	private ArrayList<QuizRecord> history;
+
+	public UserStatsHelper() {
+
+	}
+
+	public UserStatsHelper(ArrayList<QuizRecord> history) {
+		this.history = history;
+	}
+
+	public ArrayList<QuizRecord> getHistory() {
+		return history;
+	}
+
+	public void setHistory(ArrayList<QuizRecord> history) {
+		this.history = history;
+	}
+
+	public int getTotalQuizzesTaken () {
+		return history.size();
+	}
+
+	public float averageQuizPercentage () {
+
+		float totalPercent = 0;
+
+		for (QuizRecord r : history) {
+			totalPercent += r.getScorePercentage();
+		}
+
+		return totalPercent / history.size();
+	}
+
+	public float averageQuizTime() {
+
+		float totalTime = 0;
+
+		for (QuizRecord r : history) {
+			totalTime += r.getDuration();
+		}
+
+		return totalTime / history.size();
+	}
+
+
+}
